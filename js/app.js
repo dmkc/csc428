6d18331f2b0fbe48a52988527aa87467d74af880
Dmitry Kichenko <dmitrykichenko@gmail.com>
2013-02-16 16:05:42 -0500

    Add log to opacity of both maps

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
-$(function(){
+$(document).ready(function(){
     var slider = $('#slider_control'),
         map_reso = $('#map_reso'),
         map_city = $('#map_city'),
         map_city_height = map_city.height(),
 
+        // Apply log to x to for exponential opacity rolloff; 0<=x<=100
+        opacity_log = function(x){
+            return Math.log(1+(Math.exp(1)-1)*x/100);
+        },
+
         // Handle slider animation
         transition_maps = function(){
             var x = slider.val(),
                 // Exponentially adjust opacity
-                opacity = Math.log(1+(Math.exp(1)-1)*x/100)
+                //opacity_reso = x/100,
+                opacity_reso = opacity_log(x),
+                opacity_city = opacity_log(100-x)
 
-            map_city.css('opacity', 1-opacity);
-            map_reso.css('opacity',   opacity);
+
+            map_city.css('opacity', opacity_city);
+            map_reso.css('opacity', opacity_reso);
         },
         init_maps = function(){
             transition_maps();
